9dedf1ec8b0ccc28d43255a822334385abb2036c
FabriciosViana <vianasfabricio@gmail.com>
2023-05-31 13:02:24 -0300

    segundo commit

---
+++ java-basico/src/br/com/fabricioviana/syntax/Operadores.java
@@ -0,0 +1,15 @@
+package br.com.fabricioviana.syntax;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		 int a, b;
+		 a = 5;
+		 b = 6;
+		 
+		 String resultado = a==b ? "verdadeiro" : "falso"; 
+		 
+		 System.out.println(resultado);
+	}
+
+}
--- java-basico/src/br/com/fabricioviana/syntax/TiposVariaveis.java
+++ java-basico/src/br/com/fabricioviana/syntax/TiposVariaveis.java
@@ -3,7 +3,8 @@ package br.com.fabricioviana.syntax;
 public class TiposVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double salarioMinimo = 2000;
+		
 
 	}
 
